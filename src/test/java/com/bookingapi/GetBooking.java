4ec255b42f2403bfc6b8aca8f6ce3cde40a6c18b
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-07-21 00:53:55 +0530

    GetBooking

---
+++ src/test/java/com/bookingapi/GetBooking.java
@@ -0,0 +1,30 @@
+package com.bookingapi;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+public class GetBooking {
+
+    @Test
+    void getBooking(){
+
+        // Build the Request
+        RequestSpecification requestSpecification =RestAssured.given();
+        requestSpecification.baseUri("https://restful-booker.herokuapp.com");
+        requestSpecification.basePath("/booking/{id}");
+        requestSpecification.pathParams("id",1).log().all();
+
+        // Hit the Request and get the Response
+
+        Response response =requestSpecification.get();
+
+        // Validate the Response
+
+        ValidatableResponse validation = response.then().log().all();
+        validation.statusCode(200);
+        validation.extract().response().prettyPrint();
+    }
+}
