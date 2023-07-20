8f29cdd9432db03123b377a46e8b37cddd32e1f0
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-07-21 00:30:57 +0530

    Builder Patteren

--- src/test/java/builderpattern/ApiTest.java
+++ src/test/java/builderpattern/ApiTest.java
@@ -5,7 +5,7 @@ import io.restassured.specification.RequestSpecification;
 
 public class ApiTest {
     public static void main(String[] args) {
-        String baseUrl = "https://reqres.in";
+        String baseUrl = "https://reqres.in/";
         String endpoint = "/api/users";
 
         String requestBody = "{\"name\":\"morpheus\",\"job\":\"leader\"}";
@@ -23,6 +23,7 @@ public class ApiTest {
                 .extract()
                 .response()
                 .prettyPrint();
+
 
     }
 }
