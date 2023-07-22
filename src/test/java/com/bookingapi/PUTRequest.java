7e24cd68573a2690be43f0c90a865d2b4b3d6a5c
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-07-23 02:36:17 +0530

    putRequest

---
+++ src/test/java/com/bookingapi/PUTRequest.java
@@ -0,0 +1,30 @@
+package com.bookingapi;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PUTRequest {
+
+    @Test
+    void putRequest() {
+
+        HashMap<String, String> mapHeader = new HashMap<String, String>();
+        mapHeader.put("Content-Type", "application/json");
+        mapHeader.put("Accept", "application/json");
+        mapHeader.put("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=");
+        RestAssured.given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/booking/1")
+                .headers(mapHeader)
+                .body("{\"firstname\":\"James\",\"lastname\":\"Brown\",\"totalprice\":111,\"depositpaid\":true,\"bookingdates\":{\"checkin\":\"2018-01-01\",\"checkout\":\"2019-01-01\"},\"additionalneeds\":\"Breakfast\"}")
+                .when()
+                .put()
+                .then()
+                .extract()
+                .response()
+                .prettyPrint();
+    }
+}
