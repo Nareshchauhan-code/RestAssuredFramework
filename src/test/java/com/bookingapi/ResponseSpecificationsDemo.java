94e948c39fefe843fe7dbc80e9d34a4344b84057
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-07-23 12:16:51 +0530

    ResponseSpecification

--- src/test/java/com/bookingapi/ResponseSpecificationsDemo.java
+++ src/test/java/com/bookingapi/ResponseSpecificationsDemo.java
@@ -1,17 +1,17 @@
 package com.bookingapi;
 
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.hamcrest.Matchers;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
-
-public class RequestSpecificationDemo {
+public class ResponseSpecificationsDemo {
 
     RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
 
     @BeforeClass
     void setRequestSpecificationDemo() {
@@ -25,6 +25,16 @@ public class RequestSpecificationDemo {
 
     }
 
+    @BeforeClass
+    void setResponseSpecificationDemo() {
+
+        responseSpecification = RestAssured.expect();
+        responseSpecification.statusCode(200);
+        responseSpecification.time(Matchers.lessThan(5000l));
+        responseSpecification.log().all();
+
+    }
+
     @Test(priority = 1)
     void postRequest() {
 
@@ -32,10 +42,7 @@ public class RequestSpecificationDemo {
                 body("{\"firstname\":\"Jim\",\"lastname\":\"Brown\",\"totalprice\":111,\"depositpaid\":true,\"bookingdates\":{\"checkin\":\"2018-01-01\",\"checkout\":\"2019-01-01\"},\"additionalneeds\":\"Breakfast\"}")
                 .when()
                 .get()
-                .then()
-                .statusCode(200)
-                .extract()
-                .asPrettyString();
+                .then().spec(responseSpecification);
 
 
     }
