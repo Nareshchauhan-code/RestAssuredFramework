8d84025bc0d9c4615288f82c297eb6ecf83d72fe
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-07-27 01:48:25 +0530

    CreateComplexRequestUsingMapandList

--- src/test/java/PayloadCreation/CreateComplexRequestUsingMapandList.java
+++ src/test/java/PayloadCreation/CreateComplexRequestUsingMapandList.java
@@ -2,7 +2,6 @@ package PayloadCreation;
 
 import io.restassured.RestAssured;
 import org.testng.annotations.Test;
-
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
