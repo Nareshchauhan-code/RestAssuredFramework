9630c8c0bd142424777043e4279bd8aad20a2469
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-07-27 11:51:14 +0530

    Jayway Json Path

---
+++ src/test/java/setjsonpayload/ReplacethevalueinJSONDocumentUsingJsonPath.java
@@ -0,0 +1,62 @@
+package setjsonpayload;
+
+import com.jayway.jsonpath.JsonPath;
+import org.testng.annotations.Test;
+
+public class ReplacethevalueinJSONDocumentUsingJsonPath {
+
+    String payload = "{\n" +
+            "    \"store\": {\n" +
+            "        \"book\": [\n" +
+            "            {\n" +
+            "                \"category\": \"reference\",\n" +
+            "                \"author\": \"Nigel Rees\",\n" +
+            "                \"title\": \"Sayings of the Century\",\n" +
+            "                \"price\": 8.95\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"category\": \"fiction\",\n" +
+            "                \"author\": \"Evelyn Waugh\",\n" +
+            "                \"title\": \"Sword of Honour\",\n" +
+            "                \"price\": 12.99\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"category\": \"fiction\",\n" +
+            "                \"author\": \"Herman Melville\",\n" +
+            "                \"title\": \"Moby Dick\",\n" +
+            "                \"isbn\": \"0-553-21311-3\",\n" +
+            "                \"price\": 8.99\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"category\": \"fiction\",\n" +
+            "                \"author\": \"J. R. R. Tolkien\",\n" +
+            "                \"title\": \"The Lord of the Rings\",\n" +
+            "                \"isbn\": \"0-395-19395-8\",\n" +
+            "                \"price\": 22.99\n" +
+            "            }\n" +
+            "        ],\n" +
+            "        \"bicycle\": {\n" +
+            "            \"color\": \"red\",\n" +
+            "            \"price\": 19.95\n" +
+            "        }\n" +
+            "    },\n" +
+            "    \"expensive\": 10\n" +
+            "}";
+
+    @Test
+    public void replacethevalueinJSONDocumentUsingJsonPath() {
+
+        // Jayway JsonPath
+        // https://github.com/json-path/JsonPath
+
+
+        String jsonObject = JsonPath.parse(payload)
+                .set("expensive", 100)
+                .set("store.book[0].author", "Naresh Chauhan")
+                .jsonString();
+
+        System.out.println(jsonObject);
+
+
+    }
+}
