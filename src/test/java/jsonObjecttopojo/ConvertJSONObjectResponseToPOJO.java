3d8a3212f1a180e6b744b1c3bfb6db7abcd1e5cd
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-07-29 02:55:35 +0530

    ConvertJSONObjectResponseToPOJO

--- src/test/java/jsonObjecttopojo/ConvertJSONObjectResponseToPOJO.java
+++ src/test/java/jsonObjecttopojo/ConvertJSONObjectResponseToPOJO.java
@@ -8,7 +8,8 @@ public class ConvertJSONObjectResponseToPOJO {
     @Test
     public void covertJsonObjectToPojo() {
 
-        BookingPojo bookingPojo = RestAssured.get("https://run.mocky.io/v3/321140d1-3943-4bd0-a35d-e258462f8191")
+        BookingPojo bookingPojo = RestAssured
+                .get("https://run.mocky.io/v3/321140d1-3943-4bd0-a35d-e258462f8191")
                 .as(BookingPojo.class);
 
         System.out.println(bookingPojo.getJob());
